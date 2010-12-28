f4bec803f7c1b46f3a5052dde7c6981cafb46d84
Justin Rainbow <justin.rainbow@gmail.com>
2010-12-28 12:57:50 -0700

    One more change to the Doctrine caching system, changed to using the _correct_ Expires (not TimeToLive) command

--- Doctrine/Cache/RedisCache.php
+++ Doctrine/Cache/RedisCache.php
@@ -1,23 +1,4 @@
 <?php
-/*
- *  $Id$
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- * This software consists of voluntary contributions made by many individuals
- * and is licensed under the LGPL. For more information, see
- * <http://www.doctrine-project.org>.
- */
 
 namespace Bundle\RedisBundle\Doctrine\Cache;
 
@@ -27,7 +8,8 @@ use Bundle\RedisBundle\Client\Predis\LoggingConnection;
 use Predis\Commands\Get,
     Predis\Commands\Set,
     Predis\Commands\Delete,
-    Predis\Commands\TimeToLive;
+    Predis\Commands\TimeToLive,
+    Predis\Commands\Expires;
 
 /**
  * Redis cache driver.
@@ -99,7 +81,7 @@ class RedisCache extends AbstractCache
         $result = $this->_doExec($set);
         
         if ($lifeTime > 0) {
-            $ttl = new TimeToLive();
+            $ttl = new Expires();
             $ttl->setArgumentsArray(array($id, (int) $lifeTime));
             $this->_doExec($ttl);
         }
@@ -115,6 +97,14 @@ class RedisCache extends AbstractCache
         return $this->_redis->delete($id);
     }
     
+    /**
+     * Perform both the writeCommand and readResponse for a
+     * given Redis Command.
+     * 
+     * @param  object  $cmd  Redis command object
+     * 
+     * @return mixed response
+     */
     protected function _doExec($cmd)
     {
         $this->_redis->writeCommand($cmd);
