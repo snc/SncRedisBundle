c982cb531e0cedb3d5ebaf183fb82d808517dcef
H. Westphal <westphal@secure-net-concepts.de>
2011-02-12 15:40:51 +0100

    Revert "Fixed the DIC from latest Symfony update"
    This reverts commit 4d7d7823e500a6307ab131211a61889ae2a1d955.

--- DependencyInjection/RedisExtension.php
+++ DependencyInjection/RedisExtension.php
@@ -4,7 +4,6 @@ namespace Bundle\RedisBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\DependencyInjection\Loader\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
@@ -23,7 +22,7 @@ class RedisExtension extends Extension
      */
     public function configLoad(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new XmlFileLoader($container, __DIR__ . '/../Resources/config');
         $loader->load('redis.xml');
 
         $config = $this->mergeConfig($configs, $container);
@@ -159,7 +158,7 @@ class RedisExtension extends Extension
      */
     public function sessionLoad(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new XmlFileLoader($container, __DIR__ . '/../Resources/config');
         $loader->load('session.xml');
 
         $config = $this->flattenConfigs($configs);
