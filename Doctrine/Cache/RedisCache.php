37808f714df55d7a9dbe521cc868a2b5db3437f5
H. Westphal <westphal@secure-net-concepts.de>
2011-02-27 13:38:43 +0100

    Updated doctrine cache class.

--- Doctrine/Cache/RedisCache.php
+++ Doctrine/Cache/RedisCache.php
@@ -2,31 +2,31 @@
 
 namespace Snc\RedisBundle\Doctrine\Cache;
 
-use Doctrine\Common\Cache\AbstractCache;
+use Doctrine\Common\Cache\Cache;
 
 /**
- * Redis cache driver.
+ * Redis cache class
  *
  * @link    http://github.com/justinrainbow/
  * @author  Justin Rainbow <justin.rainbow@gmail.com>
  * @author  Henrik Westphal <henrik.westphal@gmail.com>
  */
-class RedisCache extends AbstractCache
+class RedisCache implements Cache
 {
     /**
      * @var \Predis\Client
      */
-    private $_redis;
+    protected $_redis;
 
     /**
      * @var string The namespace to prefix all cache ids with
      */
-    private $_namespace = null;
+    protected $_namespace = null;
 
     /**
      * @var boolean
      */
-    private $_supportsSetExpire = false;
+    protected $_supportsSetExpire = false;
 
     /**
      * Sets the redis instance to use.
@@ -40,7 +40,7 @@ class RedisCache extends AbstractCache
     }
 
     /**
-     * Gets the redis instance used by the cache.
+     * Returns the redis instance used by the cache.
      *
      * @return \Predis\Client
      */
@@ -50,60 +50,175 @@ class RedisCache extends AbstractCache
     }
 
     /**
-     * {@inheritdoc}
+     * Sets the string to prefix all cache ids with.
+     *
+     * @param string $namespace
      */
     public function setNamespace($namespace)
     {
         $this->_namespace = $namespace;
-        parent::setNamespace($namespace);
     }
 
     /**
      * {@inheritdoc}
      */
-    public function getIds()
+    public function fetch($id)
     {
-        return $this->_redis->keys($this->_namespace . '*');
+        return $this->_redis->get($this->_getNamespacedId($id));
     }
 
     /**
      * {@inheritdoc}
      */
-    protected function _doFetch($id)
+    public function contains($id)
     {
-        return $this->_redis->get($id);
+        return (bool) $this->_redis->exists($this->_getNamespacedId($id));
     }
 
     /**
      * {@inheritdoc}
      */
-    protected function _doContains($id)
+    public function save($id, $data, $lifeTime = 0)
     {
-        return (bool) $this->_redis->exists($id);
-    }
+        $id = $this->_getNamespacedId($id);
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function _doSave($id, $data, $lifeTime = 0)
-    {
         if ($this->_supportsSetExpire && 0 < $lifeTime) {
-            $result = (bool) $this->_redis->setex($id, (int) $lifeTime, $data);
+            $result = $this->_redis->setex($id, (int) $lifeTime, $data);
         } else {
-            $result = (bool) $this->_redis->set($id, $data);
-            if (0 < $lifeTime) {
-                $result = $result && (bool) $this->_redis->expire($id, (int) $lifeTime);
+            $result = $this->_redis->set($id, $data);
+            if ($result && 0 < $lifeTime) {
+                $result = $this->_redis->expire($id, (int) $lifeTime);
             }
         }
 
-        return $result;
+        return (bool) $result;
     }
 
     /**
      * {@inheritdoc}
+     */
+    public function delete($id)
+    {
+        if (false !== strpos($id, '*')) {
+            return $this->deleteByRegex('/' . str_replace('*', '.*', $id) . '/');
+        }
+
+        return $this->_doDelete($this->_getNamespacedId($id));
+    }
+
+    /**
+     * Deletes all cache entries.
+     *
+     * @return array An array of deleted cache ids
+     */
+    public function deleteAll()
+    {
+        $ids = $this->getIds();
+
+        $this->_doDelete($ids);
+
+        return $ids;
+    }
+
+    /**
+     * Deletes all cache entries matching the given regular expression.
+     *
+     * @param string $regex
+     * @return array An array of deleted cache ids
+     */
+    public function deleteByRegex($regex)
+    {
+        $deleted = array();
+
+        $ids = $this->getIds();
+
+        foreach ($ids as $id) {
+            if (preg_match($regex, $id)) {
+                $deleted[] = $id;
+            }
+        }
+
+        $this->_doDelete($deleted);
+
+        return $deleted;
+    }
+
+    /**
+     * Deletes all cache entries beginning with the given string.
+     *
+     * @param string $prefix
+     * @return array An array of deleted cache ids
+     */
+    public function deleteByPrefix($prefix)
+    {
+        $deleted = $this->getIds($prefix);
+
+        $this->_doDelete($deleted);
+
+        return $deleted;
+    }
+
+    /**
+     * Deletes all cache entries ending with the given string.
+     *
+     * @param string $suffix
+     * @return array An array of deleted cache ids
+     */
+    public function deleteBySuffix($suffix)
+    {
+        $deleted = array();
+
+        $ids = $this->getIds();
+
+        foreach ($ids as $id) {
+            if ($suffix === substr($id, -1 * strlen($suffix))) {
+                $deleted[] = $id;
+            }
+        }
+
+        $this->_doDelete($deleted);
+
+        return $deleted;
+    }
+
+    /**
+     * Returns an array of cache ids.
+     *
+     * @param string $prefix Optional id prefix
+     * @return array An array of cache ids
+     */
+    public function getIds($prefix = null)
+    {
+        if ($prefix) {
+            return $this->_redis->keys($this->_getNamespacedId($prefix) . '*');
+        } else {
+            return $this->_redis->keys($this->_getNamespacedId('*'));
+        }
+    }
+
+    /**
+     * Deletes one or more cache entries.
+     *
+     * @param string|array $id Cache id(s)
+     * @return boolean
      */
     protected function _doDelete($id)
     {
         return (bool) $this->_redis->del($id);
+    }
+
+    /**
+     * Returns the given cache id prefixed with the namespace.
+     *
+     * @param string $id Cache id
+     * @return string Prefixes cache id
+     */
+    protected function _getNamespacedId($id)
+    {
+        if ( ! $this->_namespace || 0 === strpos($id, $this->_namespace)) {
+            return $id;
+        } else {
+            return $this->_namespace . $id;
+        }
     }
 }
