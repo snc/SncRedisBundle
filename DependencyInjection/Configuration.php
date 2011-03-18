473b9e204b7288bd4a5fe68cc387c5b566027a52
H. Westphal <westphal@secure-net-concepts.de>
2011-03-18 19:49:40 +0100

    Updated configuration class

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -2,7 +2,7 @@
 
 namespace Snc\RedisBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Builder\NodeBuilder;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 
 /**
@@ -20,18 +20,22 @@ class Configuration
     public function getConfigTree()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('redis', 'array');
+        $rootNode = $treeBuilder->root('redis');
 
         $rootNode
-            ->arrayNode('class')
-                ->addDefaultsIfNotSet()
-                ->scalarNode('client')->defaultValue('Snc\RedisBundle\Client\Predis\Client')->end()
-                ->scalarNode('client_options')->defaultValue('Predis\ClientOptions')->end()
-                ->scalarNode('connection')->defaultValue('Snc\RedisBundle\Client\Predis\LoggingStreamConnection')->end()
-                ->scalarNode('connection_parameters')->defaultValue('Predis\ConnectionParameters')->end()
-                ->scalarNode('logger')->defaultValue('Snc\RedisBundle\Logger\RedisLogger')->end()
-                ->scalarNode('data_collector')->defaultValue('Snc\RedisBundle\DataCollector\RedisDataCollector')->end()
-                ->scalarNode('doctrine_cache')->defaultValue('Snc\RedisBundle\Doctrine\Cache\RedisCache')->end()
+            ->children()
+                ->arrayNode('class')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->scalarNode('client')->defaultValue('Snc\RedisBundle\Client\Predis\Client')->end()
+                        ->scalarNode('client_options')->defaultValue('Predis\ClientOptions')->end()
+                        ->scalarNode('connection')->defaultValue('Snc\RedisBundle\Client\Predis\LoggingStreamConnection')->end()
+                        ->scalarNode('connection_parameters')->defaultValue('Predis\ConnectionParameters')->end()
+                        ->scalarNode('logger')->defaultValue('Snc\RedisBundle\Logger\RedisLogger')->end()
+                        ->scalarNode('data_collector')->defaultValue('Snc\RedisBundle\DataCollector\RedisDataCollector')->end()
+                        ->scalarNode('doctrine_cache')->defaultValue('Snc\RedisBundle\Doctrine\Cache\RedisCache')->end()
+                    ->end()
+                ->end()
             ->end();
 
         $this->addConnectionsSection($rootNode);
@@ -45,32 +49,36 @@ class Configuration
     /**
      * Adds the redis.connections configuration
      *
-     * @param NodeBuilder $rootNode
+     * @param ArrayNodeDefinition $rootNode
      */
-    private function addConnectionsSection(NodeBuilder $rootNode)
+    private function addConnectionsSection(ArrayNodeDefinition $rootNode)
     {
         $rootNode
             ->fixXmlConfig('connection')
-            ->arrayNode('connections')
-                ->isRequired()
-                ->requiresAtLeastOneElement()
-                ->useAttributeAsKey('alias', false)
-                ->prototype('array')
-                    ->scalarNode('scheme')->defaultValue('tcp')->end()
-                    ->scalarNode('host')->defaultValue('127.0.0.1')->end()
-                    ->scalarNode('port')->defaultValue(6379)->end()
-                    ->scalarNode('path')->defaultNull()->end()
-                    ->scalarNode('database')->defaultValue(0)->end()
-                    ->scalarNode('password')->defaultNull()->end()
-                    ->booleanNode('connection_async')->defaultFalse()->end()
-                    ->booleanNode('connection_persistent')->defaultFalse()->end()
-                    ->scalarNode('connection_timeout')->defaultValue(5)->end()
-                    ->scalarNode('read_write_timeout')->defaultNull()->end()
-                    ->scalarNode('alias')->isRequired()->end()
-                    ->scalarNode('weight')->defaultNull()->end()
-                    ->booleanNode('iterable_multibulk')->defaultFalse()->end()
-                    ->booleanNode('throw_errors')->defaultTrue()->end()
-                    ->booleanNode('logging')->defaultFalse()->end()
+            ->children()
+                ->arrayNode('connections')
+                    ->isRequired()
+                    ->requiresAtLeastOneElement()
+                    ->useAttributeAsKey('alias', false)
+                    ->prototype('array')
+                        ->children()
+                            ->scalarNode('scheme')->defaultValue('tcp')->end()
+                            ->scalarNode('host')->defaultValue('127.0.0.1')->end()
+                            ->scalarNode('port')->defaultValue(6379)->end()
+                            ->scalarNode('path')->defaultNull()->end()
+                            ->scalarNode('database')->defaultValue(0)->end()
+                            ->scalarNode('password')->defaultNull()->end()
+                            ->booleanNode('connection_async')->defaultFalse()->end()
+                            ->booleanNode('connection_persistent')->defaultFalse()->end()
+                            ->scalarNode('connection_timeout')->defaultValue(5)->end()
+                            ->scalarNode('read_write_timeout')->defaultNull()->end()
+                            ->scalarNode('alias')->isRequired()->end()
+                            ->scalarNode('weight')->defaultNull()->end()
+                            ->booleanNode('iterable_multibulk')->defaultFalse()->end()
+                            ->booleanNode('throw_errors')->defaultTrue()->end()
+                            ->booleanNode('logging')->defaultFalse()->end()
+                        ->end()
+                    ->end()
                 ->end()
             ->end();
     }
@@ -78,30 +86,36 @@ class Configuration
     /**
      * Adds the redis.clients configuration
      *
-     * @param NodeBuilder $rootNode
+     * @param ArrayNodeDefinition $rootNode
      */
-    private function addClientsSection(NodeBuilder $rootNode)
+    private function addClientsSection(ArrayNodeDefinition $rootNode)
     {
         $rootNode
             ->fixXmlConfig('client')
-            ->arrayNode('clients')
-                ->isRequired()
-                ->requiresAtLeastOneElement()
-                ->useAttributeAsKey('alias', false)
-                ->prototype('array')
-                    ->fixXmlConfig('connection')
-                    ->arrayNode('connections')
-                        ->isRequired()
-                        ->beforeNormalization()->ifString()->then(function($v) { return (array) $v; })->end()
-                        ->prototype('scalar')->end()
-                    ->end()
-                    ->scalarNode('alias')->isRequired()->end()
-                    ->arrayNode('options')
-                        ->addDefaultsIfNotSet()
-                        ->scalarNode('profile')->defaultValue('2.0')
-                            ->beforeNormalization()
-                                ->ifTrue(function($v) { return false === is_string($v); })
-                                ->then(function($v) { return sprintf('%.1f', $v); })
+            ->children()
+                ->arrayNode('clients')
+                    ->isRequired()
+                    ->requiresAtLeastOneElement()
+                    ->useAttributeAsKey('alias', false)
+                    ->prototype('array')
+                        ->fixXmlConfig('connection')
+                        ->children()
+                            ->arrayNode('connections')
+                                ->isRequired()
+                                ->beforeNormalization()->ifString()->then(function($v) { return (array) $v; })->end()
+                                ->prototype('scalar')->end()
+                            ->end()
+                            ->scalarNode('alias')->isRequired()->end()
+                            ->arrayNode('options')
+                                ->addDefaultsIfNotSet()
+                                ->children()
+                                    ->scalarNode('profile')->defaultValue('2.0')
+                                        ->beforeNormalization()
+                                            ->ifTrue(function($v) { return false === is_string($v); })
+                                            ->then(function($v) { return sprintf('%.1f', $v); })
+                                        ->end()
+                                    ->end()
+                                ->end()
                             ->end()
                         ->end()
                     ->end()
@@ -112,37 +126,47 @@ class Configuration
     /**
      * Adds the redis.session configuration
      *
-     * @param NodeBuilder $rootNode
+     * @param ArrayNodeDefinition $rootNode
      */
-    private function addSessionSection(NodeBuilder $rootNode)
+    private function addSessionSection(ArrayNodeDefinition $rootNode)
     {
         $rootNode
-            ->arrayNode('session')
-                ->canBeUnset()
-                ->scalarNode('client')->isRequired()->end()
-                ->scalarNode('prefix')->defaultValue('session')->end()
+            ->children()
+                ->arrayNode('session')
+                    ->canBeUnset()
+                    ->children()
+                        ->scalarNode('client')->isRequired()->end()
+                        ->scalarNode('prefix')->defaultValue('session')->end()
+                    ->end()
+                ->end()
             ->end();
     }
 
     /**
      * Adds the redis.doctrine configuration
      *
-     * @param NodeBuilder $rootNode
+     * @param ArrayNodeDefinition $rootNode
      */
-    private function addDoctrineSection(NodeBuilder $rootNode)
+    private function addDoctrineSection(ArrayNodeDefinition $rootNode)
     {
-        $doctrineNode = $rootNode->arrayNode('doctrine')->canBeUnset();
+        $doctrineNode = $rootNode->children()->arrayNode('doctrine')->canBeUnset();
         foreach (array('metadata_cache', 'result_cache', 'query_cache') as $type) {
             $doctrineNode
-                ->arrayNode($type)
-                    ->canBeUnset()
-                    ->scalarNode('client')->isRequired()->end()
-                    ->scalarNode('namespace')->defaultNull()->end()
-                    ->fixXmlConfig('entity_manager')
-                    ->arrayNode('entity_managers')
-                        ->defaultValue(array('default'))
-                        ->beforeNormalization()->ifString()->then(function($v) { return (array) $v; })->end()
-                        ->prototype('scalar')->end()
+                ->children()
+                    ->arrayNode($type)
+                        ->canBeUnset()
+                        ->children()
+                            ->scalarNode('client')->isRequired()->end()
+                            ->scalarNode('namespace')->defaultNull()->end()
+                        ->end()
+                        ->fixXmlConfig('entity_manager')
+                        ->children()
+                            ->arrayNode('entity_managers')
+                                ->defaultValue(array('default'))
+                                ->beforeNormalization()->ifString()->then(function($v) { return (array) $v; })->end()
+                                ->prototype('scalar')->end()
+                            ->end()
+                        ->end()
                     ->end()
                 ->end();
         }
