3172c1c8d7b9625378a5af7b2faa3998885689c6
H. Westphal <westphal@secure-net-concepts.de>
2011-07-06 22:29:17 +0200

    Changed the configuration tree root node name.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -21,7 +21,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('redis');
+        $rootNode = $treeBuilder->root('snc_redis');
 
         $rootNode
             ->children()
