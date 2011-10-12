04b99722e0c25bfc45926cd3a1081c04a8e950ed
H. Westphal <westphal@secure-net-concepts.de>
2011-10-12 11:29:30 +0200

    Added license information (#15).

--- Tests/DependencyInjection/SncRedisExtensionTest.php
+++ Tests/DependencyInjection/SncRedisExtensionTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the SncRedisBundle package.
+ *
+ * (c) Henrik Westphal <henrik.westphal@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Snc\RedisBundle\Tests\DependencyInjection;
 
 use Snc\RedisBundle\DependencyInjection\SncRedisExtension;
