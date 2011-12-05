02d668154b60b0438e4afb4d92f36e4a2e3b6cbd
H. Westphal <westphal@secure-net-concepts.de>
2011-12-05 13:22:23 +0100

    Added missing monolog log handler class.

---
+++ Monolog/Handler/RedisHandler.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the SncRedisBundle package.
+ *
+ * (c) Henrik Westphal <henrik.westphal@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Snc\RedisBundle\Monolog\Handler;
+
+use Monolog\Handler\AbstractProcessingHandler;
+
+/**
+ * A processing handler for Monolog
+ */
+class RedisHandler extends AbstractProcessingHandler
+{
+    /**
+     * @var array
+     */
+    protected $buffer = array();
+
+    /**
+     * @var string
+     */
+    protected $key;
+
+    /**
+     * @var \Predis\Client
+     */
+    protected $redis;
+
+    /**
+     * @param \Predis\Client $redis
+     */
+    public function setRedis(\Predis\Client $redis)
+    {
+        $this->redis = $redis;
+    }
+
+    /**
+     * @param string $key
+     */
+    public function setKey($key)
+    {
+        $this->key = $key;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function close()
+    {
+        $buffer =& $this->buffer;
+        $this->redis->multiExec(function($multi) use ($buffer) {
+            foreach ($buffer as $record) {
+                $multi->rpush('monolog', $record);
+            }
+        });
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function write(array $record)
+    {
+        $this->buffer[] = (string) $record['formatted'];
+    }
+}
