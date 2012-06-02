6547515572901a0b30f8d379541c0d85af1f7578
H. Westphal <westphal@secure-net-concepts.de>
2012-06-02 10:47:23 +0200

    Added a workaround so that tests can run on travis.

--- Tests/bootstrap.sh
+++ Tests/bootstrap.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 wget --quiet http://getcomposer.org/composer.phar && \
+
+# see https://github.com/composer/composer/issues/640
+sed -i -e 's#"symfony/dependency-injection": "2.0.*",#"symfony/dependency-injection": "2.0.*", "doctrine/common": "2.1.*",#' composer.json
+
 php composer.phar install --dev && \
 mkdir Snc && ln -s ../ Snc/RedisBundle
