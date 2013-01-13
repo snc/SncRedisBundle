eca579c71de2cd23b7c1433082c3232f3f8428aa
Henrik Westphal <westphal@secotrust.de>
2013-01-13 11:51:08 +0100

    Fixed #78

--- DependencyInjection/Compiler/LoggingPass.php
+++ DependencyInjection/Compiler/LoggingPass.php
@@ -16,24 +16,25 @@ class LoggingPass implements CompilerPassInterface
     public function process(ContainerBuilder $container)
     {
         foreach ($container->findTaggedServiceIds('snc_redis.connection_parameters') as $id => $attr) {
+            $clientAlias = $attr[0]['clientAlias'];
             $parameterDefinition = $container->getDefinition($id);
             $parameters = $parameterDefinition->getArgument(0);
             if ($parameters['logging']) {
                 if ('%kernel.debug%' === $parameters['logging'] && false === $container->getParameter('kernel.debug')) {
                     continue;
                 }
-                $optionId = sprintf('snc_redis.client.%s_options', $parameters['alias']);
+                $optionId = sprintf('snc_redis.client.%s_options', $clientAlias);
                 $option = $container->getDefinition($optionId);
                 if (1 < count($option->getArguments())) {
                     throw new \RuntimeException('Please check the predis option arguments.');
                 }
                 $arguments = $option->getArgument(0);
 
-                $connectionFactoryId = sprintf('snc_redis.%s_connectionfactory', $parameters['alias']);
+                $connectionFactoryId = sprintf('snc_redis.%s_connectionfactory', $clientAlias);
                 $connectionFactoryDef = new Definition($container->getParameter('snc_redis.connection_factory.class'));
                 $connectionFactoryDef->setPublic(false);
                 $connectionFactoryDef->setScope(ContainerInterface::SCOPE_CONTAINER);
-                $connectionFactoryDef->addArgument(new Reference(sprintf('snc_redis.client.%s_profile', $parameters['alias'])));
+                $connectionFactoryDef->addArgument(new Reference(sprintf('snc_redis.client.%s_profile', $clientAlias)));
                 $connectionFactoryDef->addMethodCall('setConnectionWrapperClass', array($container->getParameter('snc_redis.connection_wrapper.class')));
                 $connectionFactoryDef->addMethodCall('setLogger', array(new Reference('snc_redis.logger')));
                 $container->setDefinition($connectionFactoryId, $connectionFactoryDef);
