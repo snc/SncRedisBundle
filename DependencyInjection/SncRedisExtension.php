6c9d8d7dff1a9d62006bba3c7f0cf395220129a9
Christian Soronellas <christian.soronellas@atrapalo.com>
2014-12-31 00:52:52 +0100

    Update for PRedis 1.0

--- DependencyInjection/SncRedisExtension.php
+++ DependencyInjection/SncRedisExtension.php
@@ -155,7 +155,7 @@ class SncRedisExtension extends Extension
 
         // TODO can be shared between clients?!
         $profileId = sprintf('snc_redis.client.%s_profile', $client['alias']);
-        $profileDef = new Definition(get_class(\Predis\Profile\ServerProfile::get($client['options']['profile']))); // TODO get_class alternative?
+        $profileDef = new Definition(get_class(\Predis\Profile\Factory::get($client['options']['profile']))); // TODO get_class alternative?
         $profileDef->setPublic(false);
         $profileDef->setScope(ContainerInterface::SCOPE_CONTAINER);
         if (null !== $client['options']['prefix']) {
@@ -241,11 +241,6 @@ class SncRedisExtension extends Extension
         }
         if ($client['options']['connection_timeout']) {
             $connectParameters[] = $client['options']['connection_timeout'];
-        } else {
-            $connectParameters[] = 0;
-        }
-        if ($client['options']['connection_persistent']) {
-            $connectParameters[] = $dsn->getPersistentId();
         }
 
         $phpredisDef->addMethodCall($connectMethod, $connectParameters);
@@ -337,7 +332,7 @@ class SncRedisExtension extends Extension
                 if ($cache['namespace']) {
                     $def->addMethodCall('setNamespace', array($cache['namespace']));
                 }
-                $container->setDefinition(sprintf('doctrine_mongodb.odm.%s_%s', $dm, $name), $def);
+                $container->setDefinition(sprintf('doctrine.odm.mongodb.%s_%s', $dm, $name), $def);
             }
         }
     }
