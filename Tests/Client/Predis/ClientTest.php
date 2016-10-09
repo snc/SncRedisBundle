f16fdf6ce5a45c7cf08183a0e62645d6649f74df
nkopliku <nelson.kopliku@gmail.com>
2016-10-09 15:03:23 +0200

    array() instead of []

--- Tests/Client/Predis/ClientTest.php
+++ Tests/Client/Predis/ClientTest.php
@@ -18,11 +18,11 @@ class ClientTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $this->client = new Client([
+        $this->client = new Client(array(
             'scheme' => 'tcp',
             'host'   => 'foo.redis.local',
             'port'   => 6379,
-        ]);
+        ));
     }
 
     /**
@@ -43,7 +43,7 @@ class ClientTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue(null === $this->client->get('foo:bar'));
         $this->assertTrue(null === $this->client->hget('foo:bar', 'foo'));
         $this->assertTrue(null === $this->client->hgetall('foo:bar'));
-        $this->assertTrue(null === $this->client->hmget('foo:bar', []));
+        $this->assertTrue(null === $this->client->hmget('foo:bar', array()));
     }
 
     public function testSetCommandString()
@@ -51,7 +51,7 @@ class ClientTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue('OK'=== $this->client->set('foo', 'bar'));
         $this->assertTrue('OK'=== $this->client->setex('foo', 10,'bar'));
         $this->assertTrue('OK'=== $this->client->psetex('foo', 10,'bar'));
-        $this->assertTrue('OK'=== $this->client->hmset('foo', ['a' => 5]));
-        $this->assertTrue('OK'=== $this->client->mset(['foo', 'bar']));
+        $this->assertTrue('OK'=== $this->client->hmset('foo', array('a' => 5)));
+        $this->assertTrue('OK'=== $this->client->mset(array('foo', 'bar')));
     }
 }
