3ea0750864a4f226206a769a1f569f496f53ae95
Richard van Laak <rvanlaak@gmail.com>
2018-12-12 09:59:50 +0100

    Update SncRedisExtension.php

--- DependencyInjection/SncRedisExtension.php
+++ DependencyInjection/SncRedisExtension.php
@@ -1,12 +1,12 @@
 <?php
 
-/*	
- * This file is part of the SncRedisBundle package.	
- *	
- * (c) Henrik Westphal <henrik.westphal@gmail.com>	
- *	
- * For the full copyright and license information, please view the LICENSE	
- * file that was distributed with this source code.	
+/*
+ * This file is part of the SncRedisBundle package.
+ *
+ * (c) Henrik Westphal <henrik.westphal@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Snc\RedisBundle\DependencyInjection;
