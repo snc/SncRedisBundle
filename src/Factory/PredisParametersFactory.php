6ebec6e05130022201e8d45497e35093dba68c26
Robbert van den Bogerd <rvdbogerd@bliq.net>
2024-02-13 12:39:31 +0100

    Insert newline

--- src/Factory/PredisParametersFactory.php
+++ src/Factory/PredisParametersFactory.php
@@ -58,6 +58,7 @@ class PredisParametersFactory
         if ($dsn->getUsername() !== null) {
             $options['username'] = $dsn->getUsername();
         }
+
         if ($dsn->getDatabase() !== null) {
             $options['database'] = $dsn->getDatabase();
         }
