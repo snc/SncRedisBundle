1e17417a118eee9def29f48fa5d38b85a4922836
Mikhail <salazar290720035017@gmail.com>
2024-07-07 15:50:39 +0300

    fix: deprecation warning on DI extension

--- src/DependencyInjection/SncRedisExtension.php
+++ src/DependencyInjection/SncRedisExtension.php
@@ -29,7 +29,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 use function array_map;
 use function assert;
