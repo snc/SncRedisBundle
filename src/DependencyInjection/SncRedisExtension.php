98ca9e661c88b663990eecd6ed815a6cc71a6723
Mikhail <salazar290720035017@gmail.com>
2024-07-08 19:24:41 +0700

    Remove deprecated usage of Symfony\Component\HttpKernel\DependencyInjection\Extension

--- src/DependencyInjection/SncRedisExtension.php
+++ src/DependencyInjection/SncRedisExtension.php
@@ -27,9 +27,9 @@ use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 use function array_map;
 use function assert;
